01b7bc45997f57d21b23798548babebcf36f430f
jemagu <magnus@gunta.se>
2015-12-01 14:02:10 +0100

    first

---
+++ src/jmx.js
@@ -0,0 +1,18 @@
+module.exports = function(RED) {
+    
+    const sonos = require("sonos");
+    const Sonos = new sonos.Sonos("127.0.0.1", 32);
+    
+    function JmxNode(config) {
+        RED.nodes.createNode(this,config);
+        this.on('input', msg => {
+            Sonos.prototype.play('http://sång.mp', (err, playing) => {
+                if (err) {
+                    this.error('knas',msg);
+                }
+            })
+            this.send(msg);
+        });
+    }
+    RED.nodes.registerType("jmx",JmxNode);
+}
