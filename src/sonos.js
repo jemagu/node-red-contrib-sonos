db6633e5f8c390b5e9296f45ec76a585f7afb8dc
jemagu <magnus@gunta.se>
2015-12-02 12:37:29 +0100

    wip

--- src/sonos.js
+++ src/sonos.js
@@ -3,15 +3,16 @@ module.exports = function(RED) {
     const sonos = require("sonos");
     
     function SonosNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
+        this.sonos_ip = config.server;
         this.on('input', msg => {
-            const sonos_device = new sonos.Sonos("127.0.0.1", 32);
-            sonos_device.play('http://sång.mp', (err, playing) => {
+            const sonos_device = new sonos.Sonos(this.sonos_ip);
+            sonos_device.play('https://archive.org/download/testmp3testfile/mpthreetest.mp3', (err, playing) => {
                 if (err) {
                     this.error(err,msg);
                 }
             })
-            this.send(msg);
+            //this.send(msg);
         });
     }
     RED.nodes.registerType("sonos",SonosNode);
