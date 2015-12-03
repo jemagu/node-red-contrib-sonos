be3f82ec90315d1e5f2dae28fa32cbc52f2ce155
jemagu <magnus@gunta.se>
2015-12-03 16:15:51 +0100

    Config properties for url and volume

--- src/sonos.js
+++ src/sonos.js
@@ -5,13 +5,19 @@ module.exports = function(RED) {
     function SonosNode(config) {
         RED.nodes.createNode(this, config);
         this.sonos_ip = config.server;
+        this.url_to_play = config.url;
         this.on('input', msg => {
             const sonos_device = new sonos.Sonos(this.sonos_ip);
-            sonos_device.play('https://archive.org/download/testmp3testfile/mpthreetest.mp3', (err, playing) => {
+            sonos_device.setVolume(this.url_to_play, (err, playing) => {
                 if (err) {
                     this.error(err,msg);
                 }
-            })
+            });
+            sonos_device.play(this.url_to_play, (err, playing) => {
+                if (err) {
+                    this.error(err,msg);
+                }
+            });
             //this.send(msg);
         });
     }
